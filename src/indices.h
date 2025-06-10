d5e9c634667b3bc29e1f2f2155b980198b2269e3
Juan Figueredo <75329583+JuanFigueredo-c@users.noreply.github.com>
2025-06-10 18:18:38 -0300

    Update indices.h Ahora compila

--- src/indices.h
+++ src/indices.h
@@ -2,13 +2,13 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-function"
 
-static inline size_t rb_idx(size_t x, size_t y, size_t dim) {
+__host__ __device__ size_t rb_idx(size_t x, size_t y, size_t dim) {
     size_t base = ((x % 2) ^ (y % 2)) * dim * (dim / 2);
     size_t offset = (x / 2) + y * (dim / 2);
     return base + offset;
 }
 
-static inline size_t idx(size_t x, size_t y, size_t stride) {
+__host__ __device__ size_t idx(size_t x, size_t y, size_t stride) {
     return x + y * stride;
 }
 
